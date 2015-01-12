8c0e7cfa99b37aac3c5a55abbaab9c328c239ca5
Wen Gu <guw@sunyit.edu>
2015-01-11 21:32:45 -0500

    Well at least the initial Version works.

--- Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/hasidic_ny_tour_app/MainActivity.java
+++ Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/hasidic_ny_tour_app/MainActivity.java
@@ -1,17 +1,54 @@
 package com.learn_hasidic_ny.hasidic_ny_tour_app;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TabHost;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
+
+    TabHost tabbedNavigationControl;
+
+    private final String toursTabTitle = "Tours";
+    private final String poiTabTitle = "POI";
+    private final String musicTabTitle = "Music";
+    private final String moreTabTitle = "More...";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Importing TabHost from layout
+        tabbedNavigationControl = (TabHost) findViewById(R.id.tabbedNavigationControl);
+
+        tabbedNavigationControl.setup();
+
+        //==============Linear and lazy addition of tab spec=================
+        TabHost.TabSpec localTabSpec = tabbedNavigationControl.newTabSpec("Tours");
+        localTabSpec.setContent(R.id.toursTab);
+        localTabSpec.setIndicator(toursTabTitle);
+        tabbedNavigationControl.addTab(localTabSpec);
+
+        localTabSpec = tabbedNavigationControl.newTabSpec(poiTabTitle);
+        localTabSpec.setContent(R.id.poiTab);
+        localTabSpec.setIndicator(poiTabTitle);
+        tabbedNavigationControl.addTab(localTabSpec);
+
+        localTabSpec = tabbedNavigationControl.newTabSpec(musicTabTitle);
+        localTabSpec.setContent(R.id.musicTab);
+        localTabSpec.setIndicator(musicTabTitle);
+        tabbedNavigationControl.addTab(localTabSpec);
+
+        localTabSpec = tabbedNavigationControl.newTabSpec(moreTabTitle);
+        localTabSpec.setContent(R.id.moreTab);
+        localTabSpec.setIndicator(moreTabTitle);
+        tabbedNavigationControl.addTab(localTabSpec);
+        //===================================================================
+
+
     }
 
 
