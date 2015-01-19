946afe7c5763bef5348625cde054e81323146b2e
Wen Gu <guw@sunyit.edu>
2015-01-19 12:41:54 -0500

    Nested Tabs are working.

--- Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/hasidic_ny_tour_app/ToursActivity.java
+++ Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/hasidic_ny_tour_app/ToursActivity.java
@@ -5,8 +5,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
-import android.widget.TabHost;
 import android.support.v4.app.FragmentTabHost;
 
 import com.learn_hasidic_ny.tours_fragments.GuidedToursFragment;
@@ -17,7 +15,7 @@ import com.learn_hasidic_ny.tours_fragments.SelfGuideToursFragment;
  */
 public class ToursActivity extends Fragment {
 
-    private FragmentTabHost tabHost;
+    private FragmentTabHost mTabHost;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,9 +25,21 @@ public class ToursActivity extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.tours_layout, container, false);
 
+        mTabHost = new FragmentTabHost(getActivity());
+        mTabHost.setup(getActivity(), getChildFragmentManager(), R.id.toursLayout);
 
-        return rootView;
+        mTabHost.addTab(mTabHost.newTabSpec("guided").setIndicator("Guided"),
+                GuidedToursFragment.class, null);
+        mTabHost.addTab(mTabHost.newTabSpec("self").setIndicator("Self Guided"),
+                SelfGuideToursFragment.class, null);
+
+        return mTabHost;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mTabHost = null;
     }
 }
--- Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/tours_fragments/SelfGuideToursFragment.java
+++ Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/tours_fragments/SelfGuideToursFragment.java
@@ -1,9 +1,26 @@
 package com.learn_hasidic_ny.tours_fragments;
 
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.learn_hasidic_ny.hasidic_ny_tour_app.R;
 
 /**
  * Created by wengu on 1/19/15.
  */
 public class SelfGuideToursFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View viewRoot = inflater.inflate(R.layout.tour_self_guided_layout, container, false);
+
+
+        return viewRoot;
+    }
 }
