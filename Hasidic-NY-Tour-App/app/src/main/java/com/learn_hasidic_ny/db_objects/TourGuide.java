4825e822fc03db3c579d5e4128841b264c78a1f0
Wen Gu <guw@sunyit.edu>
2015-01-19 13:38:03 -0500

    Adding Tour Guide Class

---
+++ Hasidic-NY-Tour-App/app/src/main/java/com/learn_hasidic_ny/db_objects/TourGuide.java
@@ -0,0 +1,24 @@
+package com.learn_hasidic_ny.db_objects;
+
+/**
+ * Created by wengu on 1/19/15.
+ */
+public class TourGuide {
+
+    //Label Tables
+    public static final String TABLE = "TourGuides";
+
+    public static final String KEY_ID = "id";
+    public static final String KEY_first_name = "first_name";
+    public static final String KEY_last_name = "last_name";
+    public static final String KEY_email = "email";
+    public static final String KEY_description = "description";
+
+    // member variables
+    public int tour_guide_ID;
+    public String first_name;
+    public String last_name;
+    public String email;
+    public String description;
+
+}
